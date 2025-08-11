1008264b5d894817ddc078254bf41173541cd6ba
emersonpessoa <eps011108@gmail.com>
2025-08-11 11:50:30 -0300

    feat: Filtering products by preço between 100 and 200 :tada:

--- sistema_gestao_estoque.sql
+++ sistema_gestao_estoque.sql
@@ -289,3 +289,16 @@ from
     inner join marca m on p.marca_id = m.id
 where
     p.preco >= 149.99;
+-- Filtrando produtos pelo preço entre 100 a 200
+select
+    p.id as codigo_produto,
+    p.descricao as descricao_produto,
+    c.descricao as descricao_categoria,
+    m.descricao as descricao_marca,
+    p.preco as preco_produto
+from
+    produto p
+    inner join categoria c on p.categoria_id = c.id
+    inner join marca m on p.marca_id = m.id
+where
+    p.preco >= 100 and p.preco <= 200;
